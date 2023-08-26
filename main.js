8fb5ee8f5ad5555083976f221a383c931575099a
harijoshi12 <mr.hariprasadjoshi@gmail.com>
2023-08-26 22:49:11 +0530

    BUG FIX TRY 1

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ import './style.css';
 import javascriptLogo from './javascript.svg';
 import viteLogo from '/marvel.svg';
 import { setupCounter } from './counter.js';
+import { MD5 } from 'crypto-js';
+
+console.log(MD5('text to hash').toString());
+console.log('env = ', import.meta.env);
 
 document.querySelector('#app').innerHTML = `
   <div>
