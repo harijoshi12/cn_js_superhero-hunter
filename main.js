e3c9b76272a41c6c245ee76f8e90f0efe7932882
harijoshi12 <mr.hariprasadjoshi@gmail.com>
2023-08-27 15:08:54 +0530

    search api integration complete

--- main.js
+++ main.js
@@ -1,28 +1,50 @@
-import './style.css';
-import javascriptLogo from './javascript.svg';
-import viteLogo from '/marvel.svg';
-import { setupCounter } from './counter.js';
-import { MD5 } from 'crypto-js';
+import MD5 from 'crypto-js/md5';
+// document.getElementsByTagName('body').s;
+// console.log(MD5('text to hash').toString());
+// console.log('env = ', import.meta.env.VITE_PUBLIC_API_KEY);
 
-console.log(MD5('text to hash').toString());
-console.log('env = ', import.meta.env);
+let timeout;
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+const fetchSuperHeroes = async (query) => {
+  const ts = new Date().getTime();
+  const pulicKey = import.meta.env.VITE_PUBLIC_API_KEY;
+  const privateKey = import.meta.env.VITE_PRIVATE_API_KEY;
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+  const hash = MD5(ts + privateKey + pulicKey).toString();
+  const apiUrl = `${baseUrl}?nameStartsWith=${query}&ts=${ts}&apikey=${pulicKey}&hash=${hash}`;
+  try {
+    const response = await fetch(apiUrl);
+    const { data } = await response.json();
+    console.log('superhreos = ', data);
+    displaySuperHeros(data?.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-setupCounter(document.querySelector('#counter'));
+const displaySuperHeros = (superHeros) => {
+  const listItems = document.getElementById('listItems');
+  listItems.innerHTML = '';
+  superHeros.forEach((hero) => {
+    const card = document.createElement('div');
+    card.className = 'card';
+    card.innerHTML = `
+    <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="heroImage" />
+      <div class="about">
+        <p>${hero.name}</p>
+      </div>
+    `;
+    listItems.appendChild(card);
+  });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const searchInput = document.querySelector('.search input');
+
+  searchInput.addEventListener('input', (e) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      fetchSuperHeroes(e.target.value);
+    }, 300);
+  });
+});
