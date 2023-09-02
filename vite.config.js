35bbdefa856d48d7caa722b7c851ee405ec2e9b8
harijoshi12 <mr.hariprasadjoshi@gmail.com>
2023-09-02 18:10:26 +0530

    multi page config try 1

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/cn_js_superhero-hunter/',
+  build: {
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, 'index.html'), // Home page
+        superhero: resolve(__dirname, 'superhero.html'), // Superhero page
+        favorites: resolve(__dirname, 'favorites.html'), // Favorites page
+      },
+    },
+  },
 });
